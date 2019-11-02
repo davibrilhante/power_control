b5c0b823d2fdb5afeb7d73857e7df1d6328363e0
Davi <davisbrilhante@gmail.com>
2019-11-02 20:28:41 -0300

    some errors corrected

--- edit-submit.sh
+++ edit-submit.sh
@@ -6,21 +6,21 @@ touch submit
 SNR=`seq 0 2 20`
 DEV=`seq 1 2 10`
 OFFSET=0.1 #`seq 0.1 0.2 1`
-ONOFF=1
+ONOFF=0
 SEED=`seq 1 50`
 #RUN=30
 DURATION=(1 60 600 900 1800 3600)
 YEARS=1
 
-for s in ${SNR[@]}
+for s in ${SNR[@]};
 	do
-	for d in ${DEV[@]}
+	for d in ${DEV[@]};
 	do
-		for x in ${SEED[@]}
+		for x in ${SEED[@]};
 		do
 			#echo -e "\nArguments = ${DURATION[2]} 30 $s 0.1 $ONOFF $d 1 \$(Process)\nQueue $RUN\n\n" >> submit;
-			touch results/output_$1_$s_$d_$x
-			echo -e "python3 $1 ${DURATION[2]} 30 $s 0.1 $ONOFF $d 1 $x >> results/output_$1_$s_$d_$x" >> submit;
+			#touch "results/output_$1_$s_$d_$x";
+			echo -e "python3 $1 ${DURATION[2]} 30 $s 0.1 $ONOFF $d 1 $x >> results/output-$1-$s-$d-$x" >> submit;
 		done
 	done
 done
